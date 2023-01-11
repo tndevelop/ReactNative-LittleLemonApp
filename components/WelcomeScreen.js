7cb904b30ba1556ee5c11cda7d9de6ad7d3d98df
TOMMASO NATTA s282478 <s282478@studenti.polito.it>
2023-01-11 09:00:04 +0100

    flatlist menu

--- components/WelcomeScreen.js
+++ components/WelcomeScreen.js
@@ -1,34 +1,55 @@
-import { View, ScrollView, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+
+const menuItemsToDisplay = [
+  { name: 'Hummus', price: '$5.00', id: '1A' },
+  { name: 'Moutabal', price: '$5.00', id: '2B' },
+  { name: 'Falafel', price: '$7.50', id: '3C' },
+  { name: 'Marinated Olives', price: '$5.00', id: '4D' },
+  { name: 'Kofta', price: '$5.00', id: '5E' },
+  { name: 'Eggplant Salad', price: '$8.50', id: '6F' },
+  { name: 'Lentil Burger', price: '$10.00', id: '7G' },
+  { name: 'Smoked Salmon', price: '$14.00', id: '8H' },
+  { name: 'Kofta Burger', price: '$11.00', id: '9I' },
+  { name: 'Turkish Kebab', price: '$15.50', id: '10J' },
+  { name: 'Fries', price: '$3.00', id: '11K' },
+  { name: 'Buttered Rice', price: '$3.00', id: '12L' },
+  { name: 'Bread Sticks', price: '$3.00', id: '13M' },
+  { name: 'Pita Pocket', price: '$3.00', id: '14N' },
+  { name: 'Lentil Soup', price: '$3.75', id: '15O' },
+  { name: 'Greek Salad', price: '$6.00', id: '16Q' },
+  { name: 'Rice Pilaf', price: '$4.00', id: '17R' },
+  { name: 'Baklava', price: '$3.00', id: '18S' },
+  { name: 'Tartufo', price: '$3.00', id: '19T' },
+  { name: 'Tiramisu', price: '$5.00', id: '20U' },
+  { name: 'Panna Cotta', price: '$5.00', id: '21V' },
+];
+
+const Item = ({name, price}) => (<View style={styles.item}>
+  
+    <Text style={styles.text}>
+        {name}       
+    </Text>
+    <Text style={{fontSize:20}}>{price}</Text>
+  
+</View>);
 
 export default function WelcomeScreen() {
+
+  const renderItem = ({item}) => <Item name={item.name} price={item.price}/>
+
   return (
     <View style={{ flex:0.78 }}>
-    <ScrollView indicatorStyle={'white'} style={styles.scroll} >
-      <Text
-        style={styles.welcome}>
-        Welcome to Little Lemon
-      </Text>
-      <Text style={styles.text}>
-      Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment. We would love to hear more about your experience with us!
-      </Text>
-    </ScrollView>
+      <FlatList data={menuItemsToDisplay} keyExtractor={it => it.id} renderItem = {renderItem}> 
+
+      </FlatList>
     </View>
   );
 }
 
 
 const styles = StyleSheet.create({
-    scroll: {
-        backgroundColor: '#333333'
-    },
-    welcome: {
-        paddingTop: 40,
-        paddingBottom:10,
-        marginTop:10,
-        fontSize: 55,
-        color: '#EDEFEE',
-        textAlign: 'center',
-        
-      },
-    text:{paddingTop: 30, fontSize:40, textAlign:'center', marginHorizontal:30, color:'#EDEFEE'}
+  
+    text:{fontSize:20, color: '#F4CE14'},
+    item:{flexDirection: 'row', justifyContent: 'space-between', paddingVertical:20, paddingHorizontal:40}
+    
 })
