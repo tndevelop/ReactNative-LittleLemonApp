f8868a8c7b2583451e81c3e5699e4813ac8d2527
TOMMASO NATTA s282478 <s282478@studenti.polito.it>
2023-01-12 12:20:29 +0100

    Login button

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -1,22 +1,52 @@
-import { ScrollView, Text, StyleSheet, TextInput} from 'react-native';
+import { ScrollView, Text, StyleSheet, TextInput, Pressable, Button} from 'react-native';
 import {useState} from 'react'
 
 export default function LoginScreen() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [logged, setLogged] = useState(false)
 
   return (
     <ScrollView style={styles.container}>
       <Text style={styles.headerText}>Welcome to Little Lemon</Text>
-      <Text style={styles.regularText}>Login to continue </Text>
-      <TextInput placeholder={'email'} style={styles.input} value={email} onChangeText={setEmail} keyboardType={'email-address'}></TextInput>
-      <TextInput placeholder={'password'} style={styles.input} value={password} onChangeText={setPassword} secureTextEntry={true}></TextInput>
 
+      {logged===false ? 
+      <>
+        
+        <Text style={styles.regularText}>Login to continue </Text>
+        <TextInput placeholder={'email'} style={styles.input} value={email} onChangeText={setEmail} keyboardType={'email-address'}></TextInput>
+        <TextInput placeholder={'password'} style={styles.input} value={password} onChangeText={setPassword} secureTextEntry={true}></TextInput>
+        <Pressable
+        style={styles.loginButton}
+          onPress={() => setLogged(!logged)}
+        >
+          <Text style={styles.buttonText}>Log in</Text>
+
+        </Pressable>
+      </>
+      :
+      <>
+        <Text style={styles.regularText}>login successful!</Text>
+      </>  
+    }
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  loginButton:{fontSize: 22,
+    padding: 10,
+    marginVertical: 20,
+    margin: 100,
+    backgroundColor: '#EE9972',
+    borderColor: '#EE9972',
+    borderWidth: 1,
+    borderRadius: 50},
+    buttonText: {
+      color: 'black',
+      textAlign: 'center',
+      fontSize: 25,
+    },
   input:{backgroundColor:'#EDEFEE', borderColor:'#EDEFEE', borderWidth:1, marginHorizontal:30, fontSize:25, marginTop:20 },
   container: {
     flex: 1,
