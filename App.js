7d0db05ce8df13821df69890cabd8b532d2df83f
TOMMASO NATTA s282478 <s282478@studenti.polito.it>
2023-01-13 12:15:54 +0100

    Tab navigation

--- App.js
+++ App.js
@@ -4,11 +4,14 @@ import LittleLemonFooter from './components/LittleLemonFooter';
 import Menu from './components/Menu';
 import WelcomeScreen from './components/WelcomeScreen';
 import LoginScreen from './components/LoginScreen';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { BottomTabBarHeightContext, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 
-const Stack = createNativeStackNavigator();
+//const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 
 export default function App() {
@@ -21,12 +24,40 @@ export default function App() {
         backgroundColor: '#495E57',
       }}>
       <LittleLemonHeader />
-      <Stack.Navigator initialRouteName="Login"
-        screenOptions={{ headerStyle: { backgroundColor: '#FBDABB' } }}>
-        <Stack.Screen name ="Menu" component={Menu} options={{ title: 'Menu' }}/>
-        <Stack.Screen name="Welcome" component={WelcomeScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator initialRouteName="Login"
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === 'Welcome') {
+            iconName = focused
+              ? 'home'
+              : 'home-outline';
+          } else if (route.name === 'Menu') {
+            iconName =  'ios-list';
+          }else if (route.name === 'Login'){
+            iconName = focused ? 'log-in' : 'log-in-outline'
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      })}>
+        {/*screenOptions={({route}) => ({
+            tabBarIcon: ({focused, color, size}) => {
+              let iconName;
+              if(route.name === 'Login'){
+                iconName == focused ? 'home' : 'home-outline'
+              }
+              return <Ionicons name={iconName} size={size} color={color} />
+            },
+            tabBarActiveTintColor: 'tomato',
+            tabBarInactiveTintColor: 'gray',
+          })
+        }>*/}
+        <Tab.Screen name ="Menu" component={Menu} options={{ title: 'Menu' }}/>
+        <Tab.Screen name="Welcome" component={WelcomeScreen} />
+        <Tab.Screen name="Login" component={LoginScreen} />
+      </Tab.Navigator>
       </View>
       <View style={styles.footerContainer}>
         <LittleLemonFooter />
